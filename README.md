e3a9517b85bf92ba31e581b8810ed825e1c858c4
Patrik Nilsson <nipatriknilsson@gmail.com>
2021-02-14 23:59:20 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,5 +1,35 @@
-# vboxcloneimmutable
-A bash-script to clone Virtualbox VMs and set them immutable.
+# vboxcloneimmutable.sh
 
-test
+The vboxcloneimmutable.sh script is made to simplify creation of VirtualBox child VMs from a parent VM. Nothing is saved when you power off a child VMs. Modifications to be used by the child VMs can only be done from the parent VM when it is running and then cloned.
+
+Syntax of calling vboxcloneimmutable.sh is
+```
+vboxcloneimmutable.sh <parent vm> <child vm_1> <child vm_2> 
+```
+
+The disk's interface is named SCSI.
+
+In sequence the script does the following:
+1) Wait for child VMs and delete them when they are not running.
+2) Set the parent's disk to vbox path with extension vdi.
+3) Run parent VM and wait for it to finish.
+4) Compact parent VM's disk. Saves space if you have zeroed out unused sectors of the virtual disk.
+5) Set the parent disk to immutable.
+6) Clone the parent into the child VMs.
+7) Set the parent's disk to NONE, so it is not accidently run.
+
+Tested on Ubuntu Mate 20.04 (Focal).
+
+# Examples
+```
+vboxcloneimmutable.sh "Ubuntu Mate Focal" "Ubuntu Mate Focal #1" "Ubuntu Mate Focal #2" "Ubuntu Mate Focal #3"
+```
+
+```
+vboxcloneimmutable.sh "Kali Linux" "Kali Linux #1" "Kali Linux #2" "Kali Linux #3"
+```
+
+```
+vboxcloneimmutable.sh "Shellter" "Shellter #1"
+```
 
---
+++ vboxcloneimmutable.sh
@@ -0,0 +1,152 @@
+#!/bin/bash
+
+set -x
+set -e
+
+parentvm="$1"
+
+storageport=0
+storagedevice=0
+storagename="SATA"
+
+
+if [ "${parentvm}" != "" ] ; then
+    shift
+
+    declare -a childvms
+
+    while [ "$1" != "" ] ; do
+        childvms+=( "$1" )
+        echo $1
+        shift
+    done
+
+    for childvm in "${childvms[@]}" ; do
+        while true; do
+            if ! vboxmanage list vms | grep -q -E "\"${childvm}\"" ; then
+                break
+            fi
+            
+            poweredoff=""
+
+            if vboxmanage showvminfo "${childvm}" 2>/dev/null 1>&2 ; then
+                poweredoff=$(vboxmanage showvminfo "${childvm}" | grep -E -c -i "State:[[:space:]]+powered off" || true)
+            fi
+            
+            if [ "$poweredoff" == "1" ] ; then
+                running=
+                running=$(ps -A -o command | grep -v grep | grep 'VirtualBoxVM' | grep -E -c "${childvm}" || true)
+                
+                if [ "$running" == "0" ] ; then
+                    break
+                fi
+
+    #            break
+            fi
+
+            sleep 10s
+        done
+
+        vboxmanage list vms | grep -q -E "\"${childvm}\"" && vboxmanage unregistervm "${childvm}" --delete || true
+    done
+
+    while true; do
+        poweredoff=""
+
+        if vboxmanage showvminfo "${parentvm}" 2>/dev/null 1>&2 ; then
+            poweredoff=$(vboxmanage showvminfo "${parentvm}" | grep -E -c -i "State:[[:space:]]+powered off" || true)
+        fi
+
+        if [ "$poweredoff" == "1" ]; then
+            break
+        fi
+
+        sleep 10s
+    done
+
+    while true ; do
+        snapshotdelete=$(vboxmanage snapshot "${parentvm}" list --machinereadable | grep -o -P  '(?<=CurrentSnapshotUUID=")[0-9a-f\-]+' || echo "")
+        if [ "$snapshotdelete" == "" ] ; then
+            break
+        fi
+
+        vboxmanage snapshot "${parentvm}" delete "$snapshotdelete"
+    done
+
+    while IFS= read f ; do
+        vartype="$(echo "$f" | grep -o -E "^(${storagename})")"
+        echo $vartype
+        varportdevice="$(echo "$f" | grep -o -i -E '\(.*\):' | tr -c '0123456789' ' ')"
+        echo $varportdevice
+        varport=$(echo "$varportdevice" | awk '{print $1}')
+        echo $varport
+        vardevice=$(echo "$varportdevice" | awk '{print $2}')
+        echo $vardevice
+
+        vboxmanage storageattach "${parentvm}" --storagectl "${vartype}" --port ${varport} --device ${vardevice} --type hdd --medium emptydrive
+    done < <(vboxmanage showvminfo "${parentvm}" | grep -E "^(${storagename})" | grep -i '(UUID:')
+
+    parentmedium=$(vboxmanage showvminfo "${parentvm}" | grep -o -P '^Config file:[[:space:]]+\K.*(\.vbox)' | sed 's/\.vbox$/\.vdi/')
+
+    while true ; do
+        childhduuid=$(vboxmanage showhdinfo "${parentmedium}" | grep -E '^Child UUIDs:' | grep -o -P '[0-9a-f\-]{36}' || echo "")
+        
+        if [ "$childhduuid" == "" ] ; then
+            break
+        fi
+
+        vboxmanage closemedium disk "$childhduuid" --delete
+    done
+
+    echo "${parentmedium}" | sed "s/\.vdi\$/\_$(date +%Y%m%d-%H%M%S)\.vdi\.bak\.tmp/" | xargs -I '{}' -- cp -a "${parentmedium}" '{}'
+
+    vboxmanage modifymedium "${parentmedium}" --type normal
+    vboxmanage storageattach "${parentvm}" --storagectl "${storagename}" --port ${storageport} --device ${storagedevice} --type hdd --medium "${parentmedium}"
+
+    vboxmanage startvm "${parentvm}"
+
+    echo "Waiting for \"${parentvm}\" to power off..."
+
+    while true ; do
+        poweredoff=""
+
+        if vboxmanage showvminfo "${parentvm}" 2>/dev/null 1>&2 ; then
+            poweredoff=$(vboxmanage showvminfo "${parentvm}" | grep -E -c -i "State:[[:space:]]+powered off" || true)
+        fi
+
+        if [ "$poweredoff" == "1" ] ; then
+            running=
+            running=$(ps -A -o command | grep -v grep | grep 'VirtualBoxVM' | grep -E -c "${parentvm}" || true)
+            
+            if [ "$running" == "0" ] ; then
+                break
+            fi
+
+#            break
+        fi
+
+        sleep 10s
+    done
+
+    vboxmanage modifymedium "$parentmedium" --compact && touch -r "$parentmedium" "${parentmedium}.stamp"
+
+    vboxmanage storageattach "${parentvm}" --storagectl "${storagename}" --port ${storageport} --device ${storagedevice} --type hdd --medium emptydrive
+
+    vboxmanage modifymedium "${parentmedium}" --type immutable
+
+    for childvm in "${childvms[@]}" ; do
+        vboxmanage clonevm "${parentvm}" --name "${childvm}" --register
+
+        vboxmanage storageattach "${childvm}" --storagectl "${storagename}" --port ${storageport} --device ${storagedevice} --type hdd --medium "${parentmedium}"
+    done
+
+    dirname "${parentmedium}" | xargs -I '{}' -- find '{}' -type f -maxdepth 1 -mindepth 1 -iname '*.vdi.bak.tmp' | LANG=C sort -r | awk '{if(NR>1) { print $0; } }' | xargs -I '{}' -- rm -f '{}'
+    
+    dirname "${parentmedium}" | xargs -I '{}' -- find '{}' -type f -maxdepth 1 -mindepth 1 -iname '*.vdi.bak.tmp' | sed "s/\.tmp\$//" | xargs -I '{}' -- mv -f "{}.tmp" '{}'
+
+    dirname "${parentmedium}" | xargs -I '{}' -- find '{}' -type f -maxdepth 1 -mindepth 1 -iname '*.vdi.bak' | LANG=C sort -r | awk '{if(NR>3) { print $0; } }' | xargs -I '{}' -- rm -f '{}'
+
+    exit 0
+fi
+
+
